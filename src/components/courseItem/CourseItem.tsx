2a2445f0c9bf252b421c5bbed4cc20cad5dcdd9d
Alvin Acosta <alvin.acosta@movon.com.ph>
2023-01-11 11:07:53 +0800

    initial confirmation modal

--- src/components/courseItem/CourseItem.tsx
+++ src/components/courseItem/CourseItem.tsx
@@ -1,32 +1,20 @@
 import { useSelector } from "react-redux";
 import { Link } from "react-router-dom";
 import { selectCurrentToken } from "../../features/auth/authSlice";
-import { useEnrollCourseMutation } from "../../features/users/userApiSlice";
 import { ICourse } from "../../types/interfaces";
 import styles from "./CourseItem.module.css";
 
 interface CourseItemProps {
   course: ICourse;
+  onAddCourseClick: (course: ICourse) => void;
 }
 
-function CourseItem({ course }: CourseItemProps) {
-  const { title, description, price, _id } = course;
+function CourseItem({ course, onAddCourseClick }: CourseItemProps) {
+  const { title, description, price } = course;
   const token = useSelector(selectCurrentToken);
 
-  const [enrollCourse, { isLoading, isError, isSuccess }] =
-    useEnrollCourseMutation();
-
-  const onAddCourseClick = async (courseId: string) => {
-    try {
-      await enrollCourse(courseId);
-      alert("course enrolled!");
-    } catch (err) {
-      console.log(err);
-    }
-  };
-
   const buttonContent = token ? (
-    <button onClick={() => onAddCourseClick(_id)}>Add Course</button>
+    <button onClick={() => onAddCourseClick(course)}>Add Course</button>
   ) : (
     <h4>
       Please <Link to={"/login"}>login</Link> to add courses.
---
+++ src/components/modal/Modal.tsx
@@ -0,0 +1,37 @@
+import { ICourse } from "../../types/interfaces";
+import styles from "./Modal.module.css";
+
+interface ModalProps {
+  course: ICourse | null;
+  addCourse: (courseId: string | undefined) => Promise<void>;
+  onCancel: () => void;
+  enrollLoading: boolean;
+}
+
+function Modal({ course, addCourse, onCancel, enrollLoading }: ModalProps) {
+  return (
+    <div className={styles.modalContainer}>
+      <div className={styles.modalDetails}>
+        <h4>Enroll to this course?</h4>
+        <h2>{course?.title}</h2>
+        <p>₱{course?.price}</p>
+      </div>
+      <div className={styles.modalButtons}>
+        <button
+          className={`${styles.btn} ${styles.btnCancel}`}
+          onClick={onCancel}
+        >
+          Cancel
+        </button>
+        <button
+          className={`${styles.btn} ${styles.btnConfirm}`}
+          onClick={() => addCourse(course?._id)}
+        >
+          {enrollLoading ? "Spinner" : "Confirm"}
+        </button>
+      </div>
+    </div>
+  );
+}
+
+export default Modal;
